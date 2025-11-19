026b677c60523724aa243091205446e780891497
maugrdev <maugrdev@hotmail.com>
2025-11-19 17:35:39 -0600

    Update server.js

--- server.js
+++ server.js
@@ -10,8 +10,9 @@ app.use(express.json());
 app.use(express.static(path.join(__dirname, "public"))); // Tu web
 
 // Rutas API
-const testimoniosRoutes = require("./routes/testimonios");
+const API = "https://maugrdev.onrender.com/api/testimonios/testimonios";
 app.use("/api/testimonios", testimoniosRoutes);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log("🚀 Servidor en puerto " + PORT));
+
