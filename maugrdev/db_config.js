e6147e54685841119abd3de6833430858e1d94bb
maugrdev <maugrdev@hotmail.com>
2025-11-19 18:51:04 -0600

    Add files via upload

--- maugrdev/db_config.js
+++ maugrdev/db_config.js
@@ -1,3 +1,14 @@
+const fs = require("fs");
+const path = require("path");
 const Database = require("better-sqlite3");
-const db = new Database("./data/mau.db");
+
+const dbPath = path.join(__dirname, "data", "mau.db");
+const dirPath = path.dirname(dbPath);
+
+// Crea la carpeta si no existe
+if (!fs.existsSync(dirPath)) {
+  fs.mkdirSync(dirPath, { recursive: true });
+}
+
+const db = new Database(dbPath);
 module.exports = db;
